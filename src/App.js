c5323b16a309451979eb1ee0ddcbbcd90b203eb2
Julia Volkova <julia.volkova789@gmail.com>
2017-12-06 16:55:19 +0300

    feat: Add components classes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,111 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
+    constructor() {
+        super();
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="App">
+            <Header />
+            <Main />
+        </div>
     );
   }
 }
 
-export default App;
+class Header extends Component {
+    render() {
+        return (
+            <div className="header-wrapper">
+                <h1>news feed</h1>
+                <p>Read top news for free</p>
+                <ul className="main-navigation">
+                    <li>World</li>
+                    <li>Politics</li>
+                    <li>Science</li>
+                    <li>Technology</li>
+                    <li>Art</li>
+                    <li>Books</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="news-wrapper">
+                <h2>top news</h2>
+                <ul className="news-grid">
+                    <li className="news">new-1</li>
+                    <li className="news">new-2</li>
+                    <li className="news">new-3</li>
+                    <li className="news">new-4</li>
+                    <li className="news">new-5</li>
+                    <li className="news">new-6</li>
+                    <li className="news">new-7</li>
+                    <li className="news">new-8</li>
+                    <li className="news">new-9</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+class CommentBox extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div className="commentBox">
+                <NewsItem />
+                <CommentForm />
+            </div>
+        )
+    }
+}
+
+class NewsItem extends Component {
+    render() {
+        return (
+            <article>
+                <h3>title</h3>
+                <time>дата публикации: 06-12-2017</time>
+                <p>something about news-item</p>
+                <a href="#" />
+                <cite>xyz</cite>
+            </article>
+        )
+    }
+}
+
+class CommentForm extends Component {
+    render() {
+        return (
+           <form>
+                <label>Add new comment</label>
+               <textarea>a left an awesome comment! I'm cool</textarea>
+               <button>post comment</button>
+           </form>
+        )
+    }
+}
+
+// class Comment extends Component {
+//     render() {
+//
+//     }
+// }
+
+// const mapStateToProps = (state) => {
+//     {store: state}
+// };
+
+export default connect()(App);
