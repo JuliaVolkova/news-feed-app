ca35ee43c0834aecd4aa408bbb8f1c4f60123bf5
Julia Volkova <julia.volkova789@gmail.com>
2017-12-06 16:56:16 +0300

    feat: Add imports, Provider, createStore

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore} from 'redux';
+import doLog from './reducer/doSomething';
+import doExample from './actions/action';
+import  { dispatch } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(doLog, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+store.dispatch(doExample(0));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
 registerServiceWorker();
