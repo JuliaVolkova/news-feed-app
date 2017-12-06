8e4e7374b9fb5a97b9eeb1c58004b3dfb139f909
Julia Volkova <julia.volkova789@gmail.com>
2017-12-06 18:34:46 +0300

    feat: Add hot-reloading

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore} from 'redux';
 import doLog from './reducer/doSomething';
 import doExample from './actions/action';
-import  { dispatch } from 'redux';
+import { dispatch } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
-import { Router, Route, Switch } from 'react-router'
+import { Router, Route, Switch } from 'react-router';
+import { AppContainer } from 'react-hot-loader';
 
 const store = createStore(doLog, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 store.dispatch(doExample(0));
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root')
-);
-registerServiceWorker();
+const render = Component => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <AppContainer>
+                <Component />
+            </AppContainer>
+        </Provider>,
+        document.getElementById('root'),
+    )
+};
+
+render(App);
+
+
+if (module.hot) {
+    module.hot.accept('./components/App', () => { render(App) })
+}
