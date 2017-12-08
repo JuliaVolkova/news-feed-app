2cd64bca1d539503c8521d9b89a9b8c43ad0a2d6
Julia Volkova <julia.volkova789@gmail.com>
2017-12-08 06:43:46 +0300

    try to understand how store.dispatch works

--- src/index.js
+++ src/index.js
@@ -5,30 +5,29 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore} from 'redux';
 import doLog from './reducer/doSomething';
-import doExample from './actions/action';
 import { dispatch } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
-import { Router, Route, Switch } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
+import doExample from './actionCreators/actionCreators';
 
 const store = createStore(doLog, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-store.dispatch(doExample(0));
+store.dispatch(doExample());
 
 const render = Component => {
     ReactDOM.render(
         <Provider store={store}>
+            <Router>
             <AppContainer>
                 <Component />
             </AppContainer>
+            </Router>
         </Provider>,
         document.getElementById('root'),
     )
 };
 
 render(App);
-
-
 if (module.hot) {
     module.hot.accept('./components/App', () => { render(App) })
 }
