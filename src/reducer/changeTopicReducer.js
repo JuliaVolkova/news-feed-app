2f46b316f0d576c5e9bf7003159d7e8ef2f94d08
Julia Volkova <julia.volkova789@gmail.com>
2017-12-14 04:11:12 +0300

    feat: Replace mock reducers

--- src/reducer/changeTopicReducer.js
+++ src/reducer/changeTopicReducer.js
@@ -1,10 +1,18 @@
-const changeTopic = (state = {}, action) => {
+export const getArticlesReducer = (state = {}, action) => {
     switch (action.type) {
-        case 'CHANGE_TOPIC':
-            return Object.assign(state, {topic: action.topic});
+        case 'GET_ARTICLES':
+            return state;
         default:
             return state;
     }
 };
 
-export default changeTopic;
+export const putArticlesToStore = (state = {}, action) => {
+    switch (action.type) {
+        case 'ARTICLES_TO_STORE':
+            const assign = Object.assign({}, {articles: action.articles});
+            return assign;
+        default:
+            return state;
+    }
+};
