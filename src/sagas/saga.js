2bfc4e825705a5d185e22103ededb5feac79a932
Julia Volkova <julia.volkova789@gmail.com>
2017-12-15 00:41:06 +0300

    feat: Add destructuring assignment for easier access to media field in given array
    Field media in receiving array of articles from API is array of objects. URL for images I need is objects in array that itself is a field of another object.
    Example: media: [{[{URL}]}]. It pretty hard to access it so I use destructuring assignment and Optonal(file in Utils) to resolve this problem.
    Option is used for checking fields in media - nullable or not they are.

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -1,24 +1,27 @@
-import {all, put, takeEvery} from "redux-saga/effects";
-import {articlesToStore} from "../actionCreators/actionCreators";
+import {all, put, takeEvery} from 'redux-saga/effects';
+import {articlesToStore} from '../actionCreators/actionCreators';
+import {fromNullable} from '../utils/option';
 
 export function* fetchArticles(action) {
-    console.log('sagaaaaa', action);
     const topic = action.topic;
     const key = '3fedd9b92cac4aa398911c2dc1350724';
     const url = `https://api.nytimes.com/svc/mostpopular/v2/mostviewed/${topic}/30.json?api-key=${key}`;
 
     const response = yield fetch(url);
     const post = yield response.json();
-    const articles = post.results.map(
-        ({title, url, byline, abstract, published_date, media}) => ({
-            title,
-            url,
-            byline,
-            abstract,
-            published_date,
-            media
-        })
-    );
+    const articles = post.results.map(({title, url, byline, abstract, published_date, media}) => {
+        let alt = 'No media presented';
+        const picture = fromNullable(media)
+            .map(m => m[0])
+            .map(m => {
+                alt = `${m.subtype} by ${m.copyright}`;
+                return m['media-metadata'];
+            })
+            .map(m => m.pop())
+            .map(({url, width, height}) => ({src: url, width, height, alt}))
+            .orElse({src: 'default_url', width: 200, height: 200, alt});
+        return {title, url, byline, abstract, published_date, media: picture}
+    });
     yield put(articlesToStore(articles))
 }
 
@@ -29,3 +32,4 @@ export function* waitGetArticles() {
 export default function* rootSaga() {
     yield all([waitGetArticles()]);
 }
+
