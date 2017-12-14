bfe0755124e7ae23953d9b7cf10c68e5ed69fdd1
Julia Volkova <julia.volkova789@gmail.com>
2017-12-15 00:37:11 +0300

    feat: Start component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,17 +1,14 @@
 import React, {Component} from 'react';
 
-class NewsItem extends Component {
-    render() {
-        return (
-            <article className="current-article">
-                <h3>title</h3>
-                <time>дата публикации: 06-12-2017</time>
-                <p>something about news-item</p>
-                <a href="#"/>
-                <cite>xyz</cite>
-            </article>
-        )
-    }
-}
+const NewsItem = ({title, url, byline, abstract, published_date}) => (
+    <article className="current-article">
+        <h3>{title}</h3>
+        <time>дата публикации: {published_date} </time>
+        <p>{abstract}</p>
+        <a href={url}/>
+        <cite>{byline}</cite>
+    </article>
+);
+
 
 export default NewsItem;
