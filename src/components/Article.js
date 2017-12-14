ab76f81b0955cf80d6cad32ac5bded2227b4b304
Julia Volkova <julia.volkova789@gmail.com>
2017-12-14 04:12:25 +0300

    feat: Add published_date

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Article = ({title, url, byline, abstract}) => (
+const Article = ({title, url, byline, abstract, published_date}) => (
     <article>
         <a href={url}/>
         <p>{title}</p>
+        <time>{published_date}</time>
         <p>{abstract}</p>
         <cite>{byline}</cite>
     </article>
