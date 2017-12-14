4fed006f13cbec7cc3ea3048f9239989c3c8a240
Julia Volkova <julia.volkova789@gmail.com>
2017-12-15 02:56:27 +0300

    feat: Add new generator for receiving one article

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -1,6 +1,21 @@
 import {all, put, takeEvery} from 'redux-saga/effects';
-import {articlesToStore} from '../actionCreators/actionCreators';
+import {articlesToStore, articleToStore} from '../actionCreators/actionCreators';
 import {fromNullable} from '../utils/option';
+
+
+function resultToArticle({title, url, byline, abstract, published_date, media}) {
+    let alt = 'No media presented';
+    const picture = fromNullable(media)
+        .map(m => m[0])
+        .map(m => {
+            alt = `${m.subtype} by ${m.copyright}`;
+            return m['media-metadata'];
+        })
+        .map(m => m.pop())
+        .map(({url, width, height}) => ({src: url, width, height, alt}))
+        .orElse({src: 'default_url', width: 200, height: 200, alt});
+    return {title, url, byline, abstract, published_date, media: picture}
+}
 
 export function* fetchArticles(action) {
     const topic = action.topic;
@@ -9,27 +24,33 @@ export function* fetchArticles(action) {
 
     const response = yield fetch(url);
     const post = yield response.json();
-    const articles = post.results.map(({title, url, byline, abstract, published_date, media}) => {
-        let alt = 'No media presented';
-        const picture = fromNullable(media)
-            .map(m => m[0])
-            .map(m => {
-                alt = `${m.subtype} by ${m.copyright}`;
-                return m['media-metadata'];
-            })
-            .map(m => m.pop())
-            .map(({url, width, height}) => ({src: url, width, height, alt}))
-            .orElse({src: 'default_url', width: 200, height: 200, alt});
-        return {title, url, byline, abstract, published_date, media: picture}
-    });
+    const articles = post.results.map(resultToArticle);
     yield put(articlesToStore(articles))
+}
+
+export function* fetchArticle(action) {
+    const topic = action.topic;
+    const key = '3fedd9b92cac4aa398911c2dc1350724';
+    const url = `https://api.nytimes.com/svc/mostpopular/v2/mostviewed/${topic}/30.json?api-key=${key}`;
+
+    const response = yield fetch(url);
+    const post = yield response.json();
+    const article = resultToArticle(post.results[action.id]);
+    yield put(articleToStore(article));
 }
 
 export function* waitGetArticles() {
     yield takeEvery('GET_ARTICLES', fetchArticles);
 }
 
+export function* waitWatchArticle() {
+    yield takeEvery('WATCH_NEWS', fetchArticle);
+}
+
 export default function* rootSaga() {
-    yield all([waitGetArticles()]);
+    yield all([
+        waitGetArticles(),
+        waitWatchArticle()
+    ]);
 }
 
