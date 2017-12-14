409d7fbf63924010a1661888c647ed5cf3370983
Julia Volkova <julia.volkova789@gmail.com>
2017-12-14 19:32:45 +0300

    feat: Add container for Main

---
+++ src/components/Main-wrapper.js
@@ -0,0 +1,15 @@
+import {connect} from "react-redux";
+import React from 'react';
+import Main from "./Main";
+import {getArticles} from "../actionCreators/actionCreators";
+
+    const mapDispatchToProps = (dispatch) => {
+        return { loadData: (topic) => dispatch(getArticles(topic))}
+    };
+
+    const mapStateToProps = (state) => {
+        console.log('Main', state);
+        return {articles: state.putArticlesToStore.articles || []};
+    };
+export const MainWrapper = connect(mapStateToProps, mapDispatchToProps)(Main);
+
