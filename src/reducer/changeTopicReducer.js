e3eac0f5e10fecd16b9c136d26c05c973a4a51d6
Julia Volkova <julia.volkova789@gmail.com>
2017-12-14 19:31:27 +0300

    fix: Merge two reducers in one

--- src/reducer/changeTopicReducer.js
+++ src/reducer/changeTopicReducer.js
@@ -1,17 +1,11 @@
-export const getArticlesReducer = (state = {}, action) => {
-    switch (action.type) {
-        case 'GET_ARTICLES':
-            return state;
-        default:
-            return state;
-    }
-};
-
 export const putArticlesToStore = (state = {}, action) => {
     switch (action.type) {
         case 'ARTICLES_TO_STORE':
             const assign = Object.assign({}, {articles: action.articles});
+            console.log( 'in reducer',action, assign);
             return assign;
+        case 'GET_ARTICLES':
+            return {};
         default:
             return state;
     }
