311acf12c14ca11209881db3b37aef9a392bcae1
Julia Volkova <julia.volkova789@gmail.com>
2017-12-15 02:53:20 +0300

    feat: Add receiving props

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-const NewsItem = ({title, url, byline, abstract, published_date}) => (
+const NewsItem = ({title, url, byline, abstract, published_date, media}) => (
     <article className="current-article">
         <h3>{title}</h3>
+        <img {...media}/>
         <time>дата публикации: {published_date} </time>
         <p>{abstract}</p>
-        <a href={url}/>
+        <a href={url}>Read more</a>
         <cite>{byline}</cite>
     </article>
 );
 
+NewsItem.propTypes = {
+    title: PropTypes.string,
+    url: PropTypes.string,
+    byline: PropTypes.string,
+    abstract: PropTypes.string,
+    published_date: PropTypes.string,
+    media: PropTypes.shape({
+        src: PropTypes.string,
+        width: PropTypes.number,
+        height: PropTypes.number,
+        alt: PropTypes.string
+    })
+};
 
 export default NewsItem;
