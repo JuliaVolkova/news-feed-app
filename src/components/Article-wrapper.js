c64220c55f666bb707cee24eacc5f4b31603e79e
Julia Volkova <julia.volkova789@gmail.com>
2017-12-15 02:50:30 +0300

    feat: Create new smart component for newsItem and comment box

--- src/components/Article-wrapper.js
+++ src/components/Article-wrapper.js
@@ -1,22 +1,35 @@
 import React, {Component} from 'react';
 import NewsItem from "./NewsItem";
 import CommentForm from "./CommentForm";
+import {connect} from "react-redux";
+import {watchNewsItem} from "../actionCreators/actionCreators";
 
 class ArticleWrapper extends Component {
     constructor(props) {
         super(props);
     }
 
+    componentWillMount(){
+        this.props.watchNewsItemOnNewPage(this.props.match.params.id, this.props.match.params.topic);
+    }
+
     render() {
+        const article = this.props.article;
         return(
             <div className="articleWrapper">
-                <NewsItem article={{}}/>
+                <NewsItem {...article}/>
                 <CommentForm/>
             </div>
         )
     }
 }
 
+const mapDispatchToProps = (dispatch) => {
+    return {watchNewsItemOnNewPage: (id, topic) => dispatch(watchNewsItem(id, topic))}
+};
 
+const mapStateToProps = (state) => {
+    return {article: state.putArticlesToStore.article || {}};
+};
 
-export default ArticleWrapper;
+export default connect(mapStateToProps, mapDispatchToProps)(ArticleWrapper);
