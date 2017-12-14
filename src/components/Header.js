e23804bf85509523afdc87e5b19582f008583b30
Julia Volkova <julia.volkova789@gmail.com>
2017-12-14 15:44:37 +0300

    feat: add prop.Types and start changing lifecycle hooks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {NavLink} from 'react-router-dom'
+import PropTypes from 'prop-types';
 
 const Header = ({topics = [], current}) => (
     <div className='header-wrapper'>
@@ -8,22 +9,40 @@ const Header = ({topics = [], current}) => (
         <p>Read top news for free</p>
         <ul className='main-navigation'>
             {topics.map((topic, index) => (
-                    <li key={index}><NavLink to={`/${topic.toLowerCase()}` }>{topic}</NavLink></li>
+                    <li key={index} ><NavLink to={`/${topic.toLowerCase()}` }>{topic}</NavLink></li>
                 )
             )}
         </ul>
     </div>
 );
 
+Header.propTypes = {
+    topics: PropTypes.array
+};
+
 const mapStateToProps = state => ({
     topics: [
+        'All-sections',
         'World',
         'Politics',
         'Science',
         'Technology',
         'Books',
-        'Art'
+        'Arts'
     ],
 });
 
 export default connect(mapStateToProps)(Header);
+
+// function mapStateToProps(state) {
+//     return {
+//         newsFeed: state.newsFeed,
+//     };
+// };
+//
+// function mapDispatchToProps(dispatch, {tag}) {
+//     dispatch(loadNews(tag || 'all-sections'));
+//     return {};
+// };
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(NewsContainer);
