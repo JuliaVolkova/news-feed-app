d587bdfcc7cf55b35e1c9f25177c6fa0a210198b
Julia Volkova <julia.volkova789@gmail.com>
2017-12-14 04:10:07 +0300

    feat: Implement fetching articles

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -1,17 +1,24 @@
-import {initializeRequest} from "../actionCreators/actionCreators";
+import {takeEvery, all, put} from "redux-saga/effects";
+import {articlesToStore} from "../actionCreators/actionCreators";
 
-export function* fetchArticles(bla) {
-    console.log('sagaaaaa', bla);
-    yield function (dispatch) {
-        dispatch(initializeRequest);
-        //     fetch('https://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/30.json?api-key=3fedd9b92cac4aa398911c2dc1350724')
-        // }
-        //     .then(response => response.json())
-        //     .then(json => {
-        //         let articlesList = json.results.map(({title, url, byline, abstract}) => ({title, url, byline, abstract}));
-        //     })
-        //     .then(
-        //         dispatch()
-        //     );
-    }
+export function* fetchArticles(action) {
+    console.log('sagaaaaa', action);
+    const topic = action.topic;
+    const key = '3fedd9b92cac4aa398911c2dc1350724';
+    const url = `https://api.nytimes.com/svc/mostpopular/v2/mostviewed/${topic}/30.json?api-key=${key}`;
+
+    const response = yield fetch(url);
+    const post = yield response.json();
+    const articles = post.results.map(
+        ({title, url, byline, abstract, published_date, media}) => ({title, url, byline, abstract, published_date, media})
+    );
+    yield put(articlesToStore(articles))
+}
+
+export function* waitGetArticles() {
+    yield takeEvery('GET_ARTICLES', fetchArticles);
+}
+
+export default function* rootSaga(){
+    yield all([waitGetArticles()]);
 }
