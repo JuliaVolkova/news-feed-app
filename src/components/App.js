b57cd5ee17542c9f95f507a16ed7c8b71c286161
Julia Volkova <julia.volkova789@gmail.com>
2017-12-15 08:58:01 +0300

    feat: Add route fot Not found page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import '../App.css';
 import {Route, Switch} from 'react-router';
-import ExampleComponent from '../example'
 import {MainWrapper} from './Main-wrapper';
-import {Header} from "./Header";
+import {Header, topics} from "./Header";
 import Redirect from "react-router-dom/es/Redirect";
 import ArticleWrapper from "./Article-wrapper";
+import Footer from "./Footer";
+import NotFound from "./Not found";
 
 class App extends Component {
     constructor(props) {
@@ -18,10 +19,11 @@ class App extends Component {
                 <Header/>
                 <Switch>
                     <Redirect exact from="/" to="/all-sections"/>
-                    <Route path="/:topic/article/:id" component={ArticleWrapper}/>
-                    <Route path="/example" component={ExampleComponent}/>
-                    <Route path="/:topic" render={(urlProps) => <MainWrapper {...urlProps}/>}/>
+                    <Route exact path="/:topic/article/:id" component={ArticleWrapper}/>
+                    <Route exact path={`/:topic(${topics.join('|')})`} render={(urlProps) => <MainWrapper {...urlProps}/>}/>
+                    <Route component={NotFound}/>
                 </Switch>
+                <Footer/>
             </div>
         );
     }
