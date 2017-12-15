181495d7220cd761868e582e2de89eb1b9d999e0
Julia Volkova <julia.volkova789@gmail.com>
2017-12-15 09:02:43 +0300

    feat: Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer>
+        <a href="https://www.nytimes.com/"><img src="/NYT-wordmark.png" /></a>
+        <p className="footer">All news were provided by The New York Times</p>
+    </footer>
+);
+
+export default Footer;
