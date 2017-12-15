97482f40821e9a90786cead39a2a43403aac29e1
Julia Volkova <julia.volkova789@gmail.com>
2017-12-15 08:51:23 +0300

    feat: Add component for animation

---
+++ src/components/planet.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+
+const Planet = () => (
+    <div className="container">
+
+        <div className="star star-1"> </div>
+        <div className="star star-2"> </div>
+        <div className="star star-3"> </div>
+
+        <div className="cloud-group-1">
+            <div className="cloud cloud-1"> </div>
+            <div className="cloud cloud-2"> </div>
+            <div className="cloud cloud-3"> </div>
+            <div className="cloud cloud-4"> </div>
+            <div className="cloud cloud-5"> </div>
+            <div className="cloud cloud-6"> </div>
+        </div>
+        <div className="cloud-group-2">
+            <div className="cloud cloud-1"> </div>
+            <div className="cloud cloud-2"> </div>
+            <div className="cloud cloud-3"> </div>
+            <div className="cloud cloud-4"> </div>
+            <div className="cloud cloud-5"> </div>
+            <div className="cloud cloud-6"> </div>
+        </div>
+        <div className="cloud-group-3">
+            <div className="cloud cloud-1"> </div>
+            <div className="cloud cloud-2"> </div>
+            <div className="cloud cloud-3"> </div>
+            <div className="cloud cloud-4"> </div>
+            <div className="cloud cloud-5"> </div>
+            <div className="cloud cloud-6"> </div>
+        </div>
+
+        <div className="orbit orbit-1"> </div>
+        <div className="orbit orbit-2"> </div>
+        <div className="orbit orbit-3"> </div>
+        <div className="orbit orbit-4"> </div>
+        <div className="orbit orbit-5"> </div>
+        <div className="orbit orbit-6"> </div>
+
+        <div className="ring-before"> </div>
+        <div className="ring-bigger-before"> </div>
+        <div className="planet"> </div>
+        <div className="ring-after"> </div>
+        <div className="ring-bigger-after"> </div>
+
+        <div className="sub-planet"> </div>
+    </div>
+);
+
+export default Planet;
