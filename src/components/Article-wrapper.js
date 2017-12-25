2071bbc96581de320f02878b764d3a59c055ba6d
Julia Volkova <julia.volkova789@gmail.com>
2017-12-25 22:51:57 +0300

    feat: Add new actions dispatch for comments

--- src/components/Article-wrapper.js
+++ src/components/Article-wrapper.js
@@ -2,34 +2,43 @@ import React, {Component} from 'react';
 import NewsItem from "./NewsItem";
 import CommentForm from "./CommentForm";
 import {connect} from "react-redux";
-import {watchNewsItem} from "../actionCreators/actionCreators";
+import {addNewComment, watchNewsItem} from "../actionCreators/actionCreators";
 
 class ArticleWrapper extends Component {
     constructor(props) {
         super(props);
     }
 
-    componentWillMount(){
+    componentWillMount() {
         this.props.watchNewsItemOnNewPage(this.props.match.params.id, this.props.match.params.topic);
     }
 
+
     render() {
         const article = this.props.article;
-        return(
+        return (
             <div className="articleWrapper">
                 <NewsItem {...article}/>
-                <CommentForm/>
+                <CommentForm addNewComment={this.props.addNewComment}/>
+                <ul className="comments">
+
+                </ul>
             </div>
         )
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {watchNewsItemOnNewPage: (id, topic) => dispatch(watchNewsItem(id, topic))}
+    return {
+        watchNewsItemOnNewPage: (id, topic) => dispatch(watchNewsItem(id, topic)),
+        addNewComment: (comment) => dispatch(addNewComment(comment))
+    }
 };
 
 const mapStateToProps = (state) => {
-    return {article: state.putArticlesToStore.article || {}};
+    return {article: state.putArticlesToStore.article || {},
+            comments: state.comments || []
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ArticleWrapper);
