3f899d920bc5748d5179d28c362eb7d2b8d43cff
Julia Volkova <julia.volkova789@gmail.com>
2017-12-27 02:00:24 +0300

    feat: Add logic for rendering comments list and dispatching ADD_COMMENTS

--- src/components/Article-wrapper.js
+++ src/components/Article-wrapper.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import NewsItem from "./NewsItem";
 import CommentForm from "./CommentForm";
 import {connect} from "react-redux";
-import {addNewComment, watchNewsItem} from "../actionCreators/actionCreators";
+import {addNewComment, getComments, watchNewsItem} from "../actionCreators/actionCreators";
 
 class ArticleWrapper extends Component {
     constructor(props) {
@@ -11,17 +11,20 @@ class ArticleWrapper extends Component {
 
     componentWillMount() {
         this.props.watchNewsItemOnNewPage(this.props.match.params.id, this.props.match.params.topic);
+        this.props.getCommentsList(this.props.article.id);
     }
-
 
     render() {
         const article = this.props.article;
+        const comments = this.props.comments;
         return (
             <div className="articleWrapper">
                 <NewsItem {...article}/>
-                <CommentForm addNewComment={this.props.addNewComment}/>
+                <CommentForm addNewComment={this.props.addNewComment(article.id)}/>
                 <ul className="comments">
-
+                    {comments.map(comment => (
+                        <li key={comment.id} className="comment-item">{comment}</li>
+                    ))}
                 </ul>
             </div>
         )
@@ -31,13 +34,15 @@ class ArticleWrapper extends Component {
 const mapDispatchToProps = (dispatch) => {
     return {
         watchNewsItemOnNewPage: (id, topic) => dispatch(watchNewsItem(id, topic)),
-        addNewComment: (comment) => dispatch(addNewComment(comment))
+        addNewComment: id => comment => dispatch(addNewComment(id, comment)),
+        getCommentsList: id => dispatch(getComments(id))
     }
 };
 
 const mapStateToProps = (state) => {
-    return {article: state.putArticlesToStore.article || {},
-            comments: state.comments || []
+    return {
+        article: state.putArticlesToStore.article || {},
+        comments: state.getComments.comments || []
     };
 };
 
